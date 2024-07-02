fff8d43ec38cadb27285ee223be7977e7a9d12c9
Abdul Rahman <abdulnsamba@gmail.com>
2024-07-02 22:48:55 +0300

    added auth and updated

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,6 +12,12 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = getenv("AUTH_TYPE", None)
+
+if auth:
+    from api.v1.auth.auth import Auth
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth() if auth == 'basic_auth' else Auth()
 
 
 @app.errorhandler(404)
@@ -19,6 +25,35 @@ def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """ Unauthorized handler
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """ Forbidden error handler
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_req():
+    """ Filter request
+    """
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/']
+    if auth:
+        auth_required = auth.require_auth(request.path, excluded_paths)
+        if auth_required:
+            if not auth.authorization_header(request):
+                abort(401)
+            if not auth.current_user(request):
+                abort(403)
 
 
 if __name__ == "__main__":
